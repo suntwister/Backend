671949a7a4a70c257b26e7a758c02c8ba5228918
Samuel Oyewusi <s.suntwister@gmail.com>
2025-10-14 11:17:35 +0100

    get.py build

--- get.py
+++ get.py
@@ -1,4 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+data = [
+    {
+        "Name": "Samuel",
+        "Track": "AI Developer"
+    }
+]
 class BasicAPI(BaseHTTPRequestHandler):
-    
+    def send_data(self, data, status = 200):
+        self.send_response(status)
+        self.send_header()
